cd3bf32aef01383a5d902bdf2e0f5c6af53ee261
orama254 <orama209@gmail.com>
2020-01-28 21:12:34 +0300

    added react practice app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dad Jokes Practice React App
+
+This is a simple react application that is meant to acquire knowledge on React Hooks, Functional Components and React Router.
---
+++ src/components/pages/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function About() {
+    return (
+    <div className="card text-center mt-3">
+            <div className="card-body">
+                <h5 className="card-title font-weight-bold">This is a Demo React Page</h5>
+                <p className="card-text">Its main focus is to learn React Hooks, React Router and Functional components</p>
+            </div>
+            <div className="card-footer text-muted">
+            Made with <i class="icon ion-heart"></i> in <a href="https://www.google.de/maps/place/Nairobi/@-1.3066374,36.762242,12z/data=!4m5!3m4!1s0x182f1172d84d49a7:0xf7cf0254b297924c!8m2!3d-1.2920659!4d36.8219462?hl=en" target="_blank" rel="noopener noreferrer">Nairobi</a> 
+            </div>
+    </div>
+    )
+}
+
+
+
+
+
+/**
+ * <div>
+            <h1>This is a Demo React Page</h1>
+            <p>Its main focus is to learn React Hooks, React Router and Functional components</p>
+        </div>
+ */
